42e78e2eb515fa3e21aa8edaabce3af683ecf058
Roshan Bharath Das <roshanbharathdas@gmail.com>
2016-07-08 12:47:29 +0200

    Added script to push to heroku server

---
+++ heroku.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+
+git checkout heroku
+echo 'Current branch : heroku'
+
+git merge master
+echo 'Merge to master done'
+
+git push heroku heroku:master
+echo 'Push to heroku server done'
+
+git checkout master
+echo 'Current branch : master'
